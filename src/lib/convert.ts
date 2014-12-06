9a3b81daab3fd2ca3134eacc1425b8b42b783865
kubosho <ta2@o2p.jp>
2014-12-06 11:02:05 +0900

    Add license notation for Convert.keyToNote()

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -1,8 +1,9 @@
 class Convert {
-  static baseMIDINum = 69;
-
-  // ref:
-  // - https://github.com/watilde/beeplay/blob/master/src/modules/nn.js
+  /**!
+   * Copyright (c) 2014 Daijiro Wachi <daijiro.wachi@gmail.com>
+   * Released under the MIT license
+   * https://github.com/watilde/beeplay/blob/master/src/modules/nn.js
+   */
   static keyToNote(name: string): number {
     var KEYS = [
       "c", "c#",
@@ -15,15 +16,15 @@ class Convert {
     ];
     var index = (name.indexOf("#") !== -1) ? 2 : 1;
     var note = name.substring(0, index).toLowerCase();
-    var number = Number(name.substring(index)) + 1;
-    return KEYS.indexOf(note) + 12 * number;
+    var num = Number(name.substring(index)) + 1;
+    return KEYS.indexOf(note) + 12 * num;
   }
 
   // ref:
   // - http://mohayonao.hatenablog.com/entry/2012/05/25/215159
   // - http://www.g200kg.com/jp/docs/tech/notefreq.html
   static noteToFreq(num: number): number {
-    return 440 * Math.pow(Math.pow(2, 1/12), num - this.baseMIDINum);
+    return 440 * Math.pow(Math.pow(2, 1/12), num - 69);
   }
 }
 
