5a3726c95dc3688cf46ead690d38f9fdcc51dbb0
kubosho <ta2@o2p.jp>
2015-01-01 21:59:54 +0900

    [add] audio.ts tests

--- test/lib/audio_test.js
+++ test/lib/audio_test.js
@@ -6,29 +6,53 @@ var context = require("../../src/lib/context");
 // frequency value reference
 // http://www.g200kg.com/jp/docs/tech/notefreq.html
 describe("Audio", function() {
-  var a = null;
+  var ctx = context();
+  var anoGakkiAudio = null;
 
   before(function() {
-    a = new audio(context());
+    anoGakkiAudio = new audio(ctx);
   });
 
   describe("#createSound(freq: number)", function() {
     it("should be frequency value is 880Hz", function() {
-      var sound = a.createSound(880);
+      var sound = anoGakkiAudio.createSound(880);
       assert(sound.frequency.value === 880);
     });
 
     it("should be frequency value is 12543.853951Hz", function() {
-      var sound = a.createSound(12543.853951);
+      var sound = anoGakkiAudio.createSound(12543.853951);
       assert(sound.frequency.value === 12543.853951);
     });
 
     it("should be sound is undefined (freq argument is zero)", function() {
-      var sound = a.createSound(0);
+      var sound = anoGakkiAudio.createSound(0);
       assert(sound === undefined);
     });
   });
 
-  describe("#connectOutput()", function() {
+  describe("#connectOutput(audio: AudioNode)", function() {
+    var sound = null;
+
+    before(function() {
+      sound = ctx.createOscillator();
+    });
+
+    it("should be working", function() {
+      assert.doesNotThrow(function() {
+        anoGakkiAudio.connectOutput(sound);
+      }, /not defined/);
+    });
+
+    it("argument is 'foo', should be throw error.", function() {
+      assert.throws(function() {
+        anoGakkiAudio.connectOutput("foo");
+      }, Error);
+    });
+
+    it("argument is {}, should be throw error.", function() {
+      assert.throws(function() {
+        anoGakkiAudio.connectOutput({});
+      }, Error);
+    });
   });
 });
