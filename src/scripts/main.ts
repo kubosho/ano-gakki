63bf90b5e0a7a0c93425134bfb7d5f9a74d6af3e
kubosho <ta2@o2p.jp>
2015-02-19 00:20:58 +0900

    [change] :art: code refactoring
    moves part of the code from an existing main() into constructor() and _touchStart().

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -5,40 +5,66 @@ import Shape = require("./shape");
 import Sound = require("./sound");
 import context = require("./context");
 
-var ctx = context.create();
-var data = new Data();
-var sound = new Sound(ctx, data.freqs);
+class Main {
+    private _ctx = context.create();
+    private _data = new Data();
+    private _sound = new Sound(this._ctx, this._data.freqs);
+    private _shape: Shape;
 
-function main() {
-    "use strict";
-
-    var currentPlayIndex = 0;
-    var sounds = sound.sounds;
-
-    var windowSize = {
-        x: window.innerWidth,
-        y: window.innerHeight
+    private static _eventTypes = ["touch", "mouse"];
+    private static _events = {
+        start: {
+            touch: "touchstart",
+            mouse: "mousedown"
+        }
     };
-    var shape = new Shape("#shape");
+    private _windowSize = { x: 0, y: 0 }
 
-    document.addEventListener("click", (ev: PointerEvent) => {
-        if (currentPlayIndex === sounds.length) {
-            currentPlayIndex = 0;
-            sound.destroySounds();
-            sounds = sound.createSounds();
+    public currentPlayIndex = 0;
+
+    constructor() {
+        Main._eventTypes.forEach((type: string) => {
+            document.addEventListener(Main._events.start[type], (<any>this), false);
+        });
+
+        this._windowSize = {
+            x: window.innerWidth,
+            y: window.innerHeight
+        };
+        this._shape = new Shape("#shape");
+    }
+
+    public handleEvent(evt: PointerEvent) {
+        switch (evt.type) {
+            case Main._events.start.touch: this._touchStart(evt, "touch"); break;
+            case Main._events.start.mouse: this._touchStart(evt, "mouse"); break;
+        }
+    }
+
+    private _touchStart(evt: PointerEvent, evtType: string): void {
+        if (evtType === "mouse") {
+            evt.preventDefault();
         }
 
-        var linePoints = data.getLinePoints(windowSize.x, windowSize.y);
-        var line = () => shape.drawLine(linePoints[currentPlayIndex]);
-        var circle = () => shape.drawCircle(ev.pageX, ev.pageY, 10);
+        var sounds = this._sound.sounds;
+
+        if (this.currentPlayIndex === sounds.length) {
+            this.currentPlayIndex = 0;
+            this._sound.destroySounds();
+            sounds = this._sound.createSounds();
+        }
+
+        var linePoints = this._data.getLinePoints(this._windowSize.x, this._windowSize.y);
+        var line = () => this._shape.drawLine(linePoints[this.currentPlayIndex]);
+        var circle = () => this._shape.drawCircle(evt.pageX, evt.pageY, 10);
 
         _.sample([line, circle])();
 
-        sound.play(sounds[currentPlayIndex]);
-        sound.stop(sounds[currentPlayIndex]);
+        this._sound.play(sounds[this.currentPlayIndex]);
+        this._sound.stop(sounds[this.currentPlayIndex]);
 
-        currentPlayIndex++;
-    });
+        this.currentPlayIndex++;
+    }
 }
 
-document.addEventListener("DOMContentLoaded", main, false);
+document.addEventListener("DOMContentLoaded", () => { new Main(); }, false);
